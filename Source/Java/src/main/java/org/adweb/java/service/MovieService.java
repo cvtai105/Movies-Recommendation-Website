288a969b4490d91ef96efb12f24edab5deab288f
MaivMinh <maivanminh.se@gmail.com>
2025-01-14 19:19:17 +0700

    fix trending

--- Source/Java/src/main/java/org/adweb/java/service/MovieService.java
+++ Source/Java/src/main/java/org/adweb/java/service/MovieService.java
@@ -11,6 +11,8 @@ import org.adweb.java.repository.Movie.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -24,56 +26,62 @@ import java.util.Optional;
 @Service
 public class MovieService {
 
-    private final MovieRepo movieRepo;
-    private final MovieUpcomingRepo movieUpcomingRepo;
-    private final MovieTrendingRepo movieTrendingRepo;
-    private final MoviePopularRepo moviePopularRepo;
-    private final MovieNowPlayingRepo movieNowPlayingRepo;
-    private final MovieTopRatedRepo movieTopRatedRepo;
-    private final MongoTemplate mongoTemplate;
+  private final MovieRepo movieRepo;
+  private final MovieUpcomingRepo movieUpcomingRepo;
+  private final MovieTrendingRepo movieTrendingRepo;
+  private final MoviePopularRepo moviePopularRepo;
+  private final MovieNowPlayingRepo movieNowPlayingRepo;
+  private final MovieTopRatedRepo movieTopRatedRepo;
+  private final MongoTemplate mongoTemplate;
 
-    public Page<Movie> getMovies(int page, int pageSize) {
-        PageRequest pageRequest = PageRequest.of(page, pageSize);
-        return movieRepo.findAll(pageRequest);
-    }
+  public Page<Movie> getMovies(int page, int pageSize) {
+    PageRequest pageRequest = PageRequest.of(page, pageSize);
+    return movieRepo.findAll(pageRequest);
+  }
 
-    public Page<MoviePopular> getMoviePopular(int page, int pageSize) {
-        PageRequest pageRequest = PageRequest.of(page, pageSize);
-        return moviePopularRepo.findAll(pageRequest);
-    }
+  public Page<MoviePopular> getMoviePopular(int page, int pageSize) {
+    PageRequest pageRequest = PageRequest.of(page, pageSize);
+    return moviePopularRepo.findAll(pageRequest);
+  }
 
-    public Page<MovieTopRated> getMovieTopRated(int page, int pageSize) {
-        PageRequest pageRequest = PageRequest.of(page, pageSize);
-        return movieTopRatedRepo.findAll(pageRequest);
-    }
+  public Page<MovieTopRated> getMovieTopRated(int page, int pageSize) {
+    PageRequest pageRequest = PageRequest.of(page, pageSize);
+    return movieTopRatedRepo.findAll(pageRequest);
+  }
 
-    public Page<MovieTrending> getMovieTrending(int page, int pageSize) {
-        PageRequest pageRequest = PageRequest.of(page, pageSize);
-        return movieTrendingRepo.findAll(pageRequest);
-    }
+  public Page<MovieTrending> getMovieTrending(int page, int pageSize) {
+    PageRequest pageRequest = PageRequest.of(page, pageSize);
+    return movieTrendingRepo.findAll(pageRequest);
+  }
 
-    public Page<MovieNowPlaying> getMovieNowPlaying(int page, int pageSize) {
-        PageRequest pageRequest = PageRequest.of(page, pageSize);
-        return movieNowPlayingRepo.findAll(pageRequest);
-    }
+  public Page<MovieNowPlaying> getMovieNowPlaying(int page, int pageSize) {
+    PageRequest pageRequest = PageRequest.of(page, pageSize);
+    return movieNowPlayingRepo.findAll(pageRequest);
+  }
 
-    public Page<MovieUpcoming> getMovieUpComing(int page, int pageSize) {
-        PageRequest pageRequest = PageRequest.of(page, pageSize);
-        return movieUpcomingRepo.findAll(pageRequest);
-    }
+  public Page<MovieUpcoming> getMovieUpComing(int page, int pageSize) {
+    PageRequest pageRequest = PageRequest.of(page, pageSize);
+    return movieUpcomingRepo.findAll(pageRequest);
+  }
 
-    public Movie getMovieDetails(Long id) {
-        return movieRepo.findByTmdbId(id)
-                .orElseThrow(
-                        () -> new RuntimeException("Movie not found with tmdb_id " + id)
-                );
-    }
+  public Movie getMovieDetails(Long id) {
+    return movieRepo.findByTmdbId(id)
+            .orElseThrow(
+                    () -> new RuntimeException("Movie not found with tmdb_id " + id)
+            );
+  }
 
-    public Review addReview(Long tmdbId, Review review) {
-        review.setCreatedAt(LocalDateTime.now().toString());
-        Query query = new Query(Criteria.where("tmdb_id").is(tmdbId));
-        Update update = new Update().push("reviews", review);
-        mongoTemplate.updateFirst(query, update, Movie.class);
-        return review;
-    }
+  public Review addReview(Long tmdbId, Review review) {
+    review.setCreatedAt(LocalDateTime.now().toString());
+    Query query = new Query(Criteria.where("tmdb_id").is(tmdbId));
+    Update update = new Update().push("reviews", review);
+    mongoTemplate.updateFirst(query, update, Movie.class);
+    return review;
+  }
+
+  public Page<Movie> findMoviesWithLatestTrailer(int page, int pageSize) {
+    Sort sort = Sort.by(Sort.Direction.DESC, "release_date");
+    Pageable pageable = PageRequest.of(page, pageSize, sort);
+    return movieRepo.findMoviesWithLatestTrailer(pageable);
+  }
 }
